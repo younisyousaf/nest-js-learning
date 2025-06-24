9ce2a21b9b1b55687fb57c2e78c465f729abd64d
Younis <yykhan789@outlook.com>
2025-06-24 17:18:51 +0500

    reset project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './hello/hello.module';
-import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import Joi, * as joi from 'joi'
-import appConfig from './config/app.config';
 
 // root module -> use all the sub modules
 
 @Module({
-    imports: [
-        //it will load the .env file
-        ConfigModule.forRoot({
-            isGlobal: true, //makes config module available globally
-            // validationSchema: joi.object({
-            //     PORT: Joi.number().required(),
-            // })
-            load: [appConfig]
-        }),
-        HelloModule, UserModule],
+    imports: [],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { log } from 'console';
 
 @Injectable()
 export class AppService {
-    constructor(
-        private configService: ConfigService
-    ) { }
     getHello(): string {
-        // const port = this.configService.get('PORT', '5000');
-        const port = this.configService.get('PORT');
-        console.log('PORT', port);
         return 'Hello World!';
     }
 }
